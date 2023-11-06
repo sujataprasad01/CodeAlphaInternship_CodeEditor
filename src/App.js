e33664e3c7d7cf9fd471141a9b74178faee52fdf
sujataprasad01 <sujataprasad2004@gmail.com>
2023-11-06 17:25:12 +0530

    done with home page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import EditorPage from './components/EditorPage';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <BrowserRouter>
+    <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route
+            path="/editor/:roomId"
+            element={<EditorPage />}
+        ></Route>
+    </Routes>
+</BrowserRouter>
   );
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,56 @@
 import React from 'react'
-
+import logo from '../images/code (1).png'
 const Home = () => {
   return (
-    <div>
-      Home
+    <div className="homePageWrapper">
+    <div className="formWrapper">
+        <div className="editorname">
+        <img
+            className="homePageLogo"
+            src={logo}
+            alt="code-sync-logo"
+        /><h1>Code Sync</h1>
+        </div>
+        <h4 className="mainLabel">Paste invitation ROOM ID</h4>
+        <div className="inputGroup">
+            <input
+                type="text"
+                className="inputBox"
+                placeholder="ROOM ID"
+                // onChange={(e) => setRoomId(e.target.value)}
+                // value={roomId}
+                // onKeyUp={handleInputEnter}
+            />
+            <input
+                type="text"
+                className="inputBox"
+                placeholder="USERNAME"
+                // onChange={(e) => setUsername(e.target.value)}
+                // value={username}
+                // onKeyUp={handleInputEnter}
+            />
+            <button className="btn joinBtn">
+                Join
+            </button>
+            <span className="createInfo">
+                If you don't have an invite then create &nbsp;
+                <a
+                    // onClick={createNewRoom}
+                    href=""
+                    className="createNewBtn"
+                >
+                    new room
+                </a>
+            </span>
+        </div>
     </div>
+    <footer>
+        <h4>
+            Built with 💛 &nbsp; by &nbsp;
+            <a href="https://github.com/sujataprsad01">Sujata Prasad</a>
+        </h4>
+    </footer>
+</div>
   )
 }
 
