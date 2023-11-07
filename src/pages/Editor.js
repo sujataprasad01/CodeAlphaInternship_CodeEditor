516fb3e805bc518d01fb65ff8de7628baa529c32
sujataprasad01 <sujataprasad2004@gmail.com>
2023-11-07 11:29:12 +0530

    done with textrea using codemirror

--- src/pages/Editor.js
+++ src/pages/Editor.js
@@ -1,11 +1,26 @@
-import React from 'react'
-
+import React, { useEffect } from 'react'
+import Codemirror from 'codemirror'
+import 'codemirror/lib/codemirror.css'
+import 'codemirror/mode/javascript/javascript'
+import 'codemirror/theme/dracula.css'
+import 'codemirror/addon/edit/closetag'
+import 'codemirror/addon/edit/closebrackets'
 const Editor = () => {
-  return (
-    <div>
-      editor
-    </div>
-  )
-}
+    useEffect(()=>{
+        async function init(){
+            Codemirror.fromTextArea(document.getElementById('realtime'),{
+                node:{name:'javascript', json:true},
+                theme:"dracula",
+                autoCloseTags:true,
+                autoCloseBrackets:true,
+                lineNumbers:true,
+    });
+    }
+    init();
+   }, []); 
+
+  return <textarea id="realtime"></textarea>;
+  
+};
 
 export default Editor
